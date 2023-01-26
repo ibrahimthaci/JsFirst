ef42c99e3c2fc4705dea1b63e9ef84db4f3d2e55
ibrahimthaci <ibrahim_thaqi@outlook.com>
2023-01-26 12:44:30 +0100

    Guess number game

---
+++ GuessNumber/index.js
@@ -0,0 +1,60 @@
+"use strict";
+
+let Realnumber = Math.floor(Math.random() * 20) + 1;
+console.log(Realnumber);
+let score = 20;
+let hightscore = "";
+
+document.querySelector(".checkButton").addEventListener("click", function () {
+  let guess = parseInt(document.querySelector(".guess").value);
+  console.log(guess);
+
+  //No input
+  if (!guess) {
+    console.log("wrong number");
+    document.querySelector("#first").textContent =
+      "No number, please add number😁";
+
+    //right guess
+  } else if (guess === Realnumber) {
+    document.querySelector(".buttonNumber").textContent = Realnumber;
+    document.querySelector("#first").textContent = "Bravooo 😘👌";
+    document.querySelector("#HeaderGuess").textContent =
+      "You Guuesssed Righttt 😘👌";
+    document.querySelector("body").style.background = "green";
+    if (score > hightscore) hightscore = score;
+    document.querySelector("#third").textContent =
+      "Higher Score: " + hightscore + "🤡";
+  }
+
+  // Wrong number
+  else if (guess !== Realnumber) {
+    if (score > 1) {
+      document.querySelector("#first").textContent =
+        guess > Realnumber ? "Look for Lower Number" : "Look for Higher Number";
+      score--;
+      document.querySelector("#second").textContent = "Score: " + score;
+    } else {
+      document.querySelector("#first").textContent = "You lost the game 😕";
+      document.querySelector("#HeaderGuess").textContent = "You Lost 😕";
+      document.querySelector("#second").textContent =
+        "Your score is " + 0 + " and number was " + Realnumber;
+      document.querySelector(".buttonNumber").textContent = Realnumber;
+    }
+  }
+});
+
+document.querySelector(".resetBtn").addEventListener("click", function () {
+  score = 20;
+  hightscore = 0;
+  Realnumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector(".guess").value = "";
+  document.querySelector("#HeaderGuess").textContent = "Guess My number";
+  document.querySelector("#first").textContent = "Start Guessing ... 😍";
+  document.querySelector("#second").textContent = "Score :" + score;
+  document.querySelector("#third").textContent = "Higher Score: " + hightscore;
+  document.querySelector(".buttonNumber").textContent = "";
+  document.querySelector("body").style.background = "white";
+  document.querySelector(".buttonNumber").textContent = "?";
+});
--- HeadOrTail/index.js
+++ HeadOrTail/index.js
@@ -1,42 +1,40 @@
-'use scrict'
+"use scrict";
 
-let input = document.querySelector('.input')
-let message = document.querySelector('.output')
-const winner= document.querySelector('.winner');
-let btn = document.querySelectorAll('button')
+let input = document.querySelector(".input");
+let message = document.querySelector(".output");
+const winner = document.querySelector(".winner");
+let btn = document.querySelectorAll("button");
 
 let coin = ["Head", "Tail"];
-let score = [0,0];
+let score = [0, 0];
 let WinnerMessage;
 
-
 for (let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener('click', GameFunction)
+  btn[i].addEventListener("click", GameFunction);
 }
 
-function GameFunction(e){
-    let Human = e.target.innerText
-    let Computer = Math.floor(Math.random()*coin.length)
-    let ComputerGuess = coin[Computer]
-    let playerWon = 0;
+function GameFunction(e) {
+  let Human = e.target.innerText;
+  let Computer = Math.floor(Math.random() * coin.length);
+  let ComputerGuess = coin[Computer];
+  let playerWon = 0;
 
-    message.innerHTML = "Computer selected " + ComputerGuess + "<br>"
-    
-    if (Human === ComputerGuess){
-        WinnerMessage = "Human won "
-        score[0]++
-    }
-    else{
-        WinnerMessage = "Computer won "
-        score[1]++
-    }
-    message.innerHTML += "Player score is "+score[0]+ " Computer score is "+ score[1];
-     
-    if (Human === ComputerGuess){
-     winner.style.color ="White";
-    }
-     else{
-        winner.style.color ="red";
-    }
-    winner.innerHTML = WinnerMessage 
+  message.innerHTML = "Computer selected " + ComputerGuess + "<br>";
+
+  if (Human === ComputerGuess) {
+    WinnerMessage = "Human won ";
+    score[0]++;
+  } else {
+    WinnerMessage = "Computer won ";
+    score[1]++;
+  }
+  message.innerHTML +=
+    "Player score is " + score[0] + " Computer score is " + score[1];
+
+  if (Human === ComputerGuess) {
+    winner.style.color = "White";
+  } else {
+    winner.style.color = "red";
+  }
+  winner.innerHTML = WinnerMessage;
 }
