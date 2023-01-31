a21d1543da8917b7c47753dd62ccdcf17ab70798
ibrahimthaci <ibrahim_thaqi@outlook.com>
2023-01-31 13:37:42 +0100

    before update

--- RollingDice/index.js
+++ RollingDice/index.js
@@ -5,12 +5,11 @@ const theDice = document.querySelector(".theDice");
 const RollDice = document.querySelector(".rollDice");
 const holdDice = document.querySelector(".holdDice");
 
-let totalResult1 = document.querySelector('.Player-0-result')
-let totalResult2 = document.querySelector('.Player-1-result')
+let totalResult1 = document.querySelector(".Player-0-result");
+let totalResult2 = document.querySelector(".Player-1-result");
 
-let curentRes1 = document.querySelector('.Current-0')
-let curentRes2 = document.querySelector('.Current-1')
-
+let curentRes1 = document.querySelector(".Current-0");
+let curentRes2 = document.querySelector(".Current-1");
 
 const scores = [0, 0];
 let currentScore = 0;
@@ -23,15 +22,14 @@ RollDice.addEventListener("click", function () {
   theDice.classList.remove("HideDice");
   theDice.src = `./svg/dice${dice}.svg`;
 
-  if (dice !== 1 ) {
+  if (dice !== 1) {
     currentScore += dice;
-    document.querySelector(`.Current-${activePlayer}`).textContent=currentScore;
-    scores[activePlayer]= currentScore;
-    totalResult2.textContent = scores[activePlayer];
-    
+    document.querySelector(`.Current-${activePlayer}`).textContent =
+      currentScore;
+    scores[activePlayer] = currentScore;
   } else {
-    document.querySelector(`.Current-${activePlayer}`).textContent=0;
+    document.querySelector(`.Current-${activePlayer}`).textContent = 0;
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
-}
+  }
 });
